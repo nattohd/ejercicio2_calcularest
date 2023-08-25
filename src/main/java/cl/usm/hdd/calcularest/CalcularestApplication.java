b19de5a42f4fe0745d34c93d70403a5f18ef6156
NattoHD <tubepika@gmail.com>
2023-08-25 15:39:19 -0400

    first commit

---
+++ src/main/java/cl/usm/hdd/calcularest/CalcularestApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.calcularest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcularestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcularestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usm/hdd/calcularest/CalcularestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.calcularest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcularestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
